f5db38430e45c4fab23a59130b080362976d8656
mahajanvishal123 <mahajanvishal9644@gmail.com>
2025-02-04 22:34:30 +0530

    changes

--- src/Component/Header.js
+++ src/Component/Header.js
@@ -14,45 +14,54 @@ const Header = () => {
     return () => window.removeEventListener("resize", handleResize);
   }, []);
 
+
+
+  const [isOpen, setIsOpen] = useState(false);
+
   const handleToggle = () => {
-    setToggleheader(!toggleheader);
+    setIsOpen(!isOpen);
   };
 
   return (
     <div className="container-fluid">
-      <header className="mt-3 header d-flex justify-content-between align-items-center">
-        <div className="d-flex align-items-center">
-          <Link to="/home">
-          <img 
-            src="https://i.ibb.co/5Tq0pq6/Black-logo-no-background.png" 
-            alt="Logo" 
-            className="me-2 " 
-            width="50%"
+   <header className="mt-3 header container-fluid">
+      {/* Top row: Logo and Toggle Button */}
+      <div className="d-flex justify-content-between align-items-center">
+        <Link to="/home">
+          <img
+            src="https://i.ibb.co/5Tq0pq6/Black-logo-no-background.png"
+            alt="Logo"
+            className="me-2"
+            width="150"
           />
-          </Link>
-        </div>
-        <div className="nav-conten-men">
-        <div className='d-flex justify-content-between ' >
-          <div className='d-flex align-items-center justify-content-between gap-2 mx-4 header-content-p'>
-           <Link className='header-content-p' to="/features" ><p>Features</p> </Link>
-           <Link className='header-content-p' to="/pricing" ><p>Pricing</p> </Link>
-           <Link className='header-content-p' to="/integration" > <p>Integrations</p></Link>
-           <Link className='header-content-p' to="/contact" >  <p>Contact</p></Link>
-
+        </Link>
+        {/* Toggle button visible only on mobile */}
+        <button
+          className="navbar-toggler d-lg-none"
+          type="button"
+          onClick={handleToggle}
+        >
+          <i className="bi bi-list"></i>
+        </button>
+      </div>
+      
+      {/* Navigation content */}
+      <div className={`${isOpen ? "d-block" : "d-none"} d-lg-flex mt-2`}>
+        <div className="d-flex flex-column flex-lg-row justify-content-between align-items-lg-center w-100">
+          <div className="d-flex flex-column flex-lg-row align-items-center gap-2 mx-4 header-content-p">
+            <Link className="header-content-p" to="/features"><p>Features</p></Link>
+            <Link className="header-content-p" to="/pricing"><p>Pricing</p></Link>
+            <Link className="header-content-p" to="/integration"><p>Integrations</p></Link>
+            <Link className="header-content-p" to="/contact"><p>Contact</p></Link>
           </div>
-        <div className='d-flex sign_in'>
-         
-          <button className="btn btn-outline-dark me-3">Sign in</button>
-          <button className="btn btn-img-color">Sign up free</button>
-          <i className="bi bi-list m-2" onClick={handleToggle}></i>
-        </div>
+          <div className="d-flex sign_in mt-3 mt-lg-0 mx-4">
+            <button className="btn btn-outline-dark me-3" style={{height:'35px'}}>Sign in</button>
+            <button className="btn btn-primary" style={{height:'35px'}}>Sign up free</button>
           </div>
-
-         
         </div>
-      </header>
-
-      {toggleheader && isMobile && 
+      </div>
+    </header>
+       {/* {toggleheader && isMobile && 
                 <ul className="navbar-nav ms-auto">
                 <li className="nav-item">
                   <Link to='/features' >
@@ -75,7 +84,7 @@ const Header = () => {
                     </Link>
                 </li>
                
-              </ul>          }
+              </ul>          }  */}
       {/* Render Navbar inline or as a sidebar */}
       {/* {toggleheader && (
         <div className={isMobile ? 
