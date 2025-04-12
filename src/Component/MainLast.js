b5f6b0f5dfd054a51b05bbc9387715eb2211a06e
Arun-badode <arunbadode08@gmail.com>
2025-04-12 14:45:01 +0530

    changes

--- src/Component/MainLast.js
+++ src/Component/MainLast.js
@@ -18,12 +18,12 @@ const MainLast = () => {
   };
 
   return (
-   
+
     <div class="container mt-5">
-       {showModal && (
+      {showModal && (
         <div className="modal-backdrop" onClick={handleCloseModal}>
-          <div 
-            className="video-modal-content" 
+          <div
+            className="video-modal-content"
             onClick={(e) => e.stopPropagation()}
           >
             <button className="close-modal-btn" onClick={handleCloseModal}>×</button>
@@ -42,28 +42,28 @@ const MainLast = () => {
           </div>
         </div>
       )}
-        <div class="row">
+      <div class="row">
         <div className='col-md-6 shadow-sm border rounded gap-2'>
-                
-                <div className="col-md-12 shadow-sm border rounded">
-       
-            <div 
-              className=" video-thumbnail-container position-relative" 
+
+          <div className="col-md-12 shadow-sm border rounded">
+
+            <div
+              className=" video-thumbnail-container position-relative"
               onClick={() => handleOpenVideoModal("https://media-hosting.imagekit.io/d0d27d3aff7e4b5f/Files.mp4?Expires=1838892048&Key-Pair-Id=K2ZIVPTIP2VGHC&Signature=Uf0bJxRHZr7Uy4WDVRbZHH0Z0Glu5962kkMxmSm96VM8ZXFRCl6gDmHo8dhsnkKwcNpzH9emtJxNse2U~wpkD0tV~R-LVHXkK6gfVtftyXIA2s1XrGGM-sKeHZB0XneSjoHcy5wwhiqqvcJhsIfVV85aOauyVDFq1XomgGG-pc-aChuBua4PQ6J02zOH2091x9CGfk6HGhuBftJANNWGaRb-H0KL3tzyVEQmCqssktPFVysdCDyQ0AP1BSdYkbLPcGWSGDkamKWE2RtG1Ze9lLEXYrpr6IBviNcR6VaKF4L8ICE-auAfZCZpv3J2Gd1ZoSH1FLJI2wQ9QIp7oMHovA__")}
               style={{ cursor: 'pointer' }}
             >
               <span className='visual__figure'>Manage Files   </span>
               {/* Video thumbnail image */}
-              
-                <img 
-                  src="https://i.ibb.co/k21DnXgR/Screenshot-2025-04-10-144707.png" 
-                  alt="Video thumbnail" 
-                  className="w-100 h-100 rounded mt-8"
-                />
-              
-              
+
+              <img
+                src="https://i.ibb.co/k21DnXgR/Screenshot-2025-04-10-144707.png"
+                alt="Video thumbnail"
+                className="w-100 h-100 rounded mt-8"
+              />
+
+
               {/* Play button overlay */}
-              <div 
+              <div
                 className="play-button-overlay position-absolute"
                 style={{
                   top: '50%',
@@ -78,7 +78,7 @@ const MainLast = () => {
                   alignItems: 'center'
                 }}
               >
-                <div 
+                <div
                   style={{
                     width: '0',
                     height: '0',
@@ -91,16 +91,16 @@ const MainLast = () => {
               </div>
             </div>
           </div>
-                </div>
-                <div className="col-md-6 d-flex align-items-center">
-            <div className="text">
-              <h1 className="fw-bold">Files</h1>
-              <p className="fs-5">
+        </div>
+        <div className="col-md-6 d-flex align-items-center">
+          <div className="text">
+            <h1 className="fw-bold">Files</h1>
+            <p className="fs-5">
               Upload, manage, and share documents securely. Set permissions, track versions, and keep everything organized with tags and folders.
-              </p>
-            </div>
+            </p>
           </div>
         </div>
+      </div>
     </div>
   )
 }
